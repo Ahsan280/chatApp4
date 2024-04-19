3fcd0494bfc908a5da833d321afc7b23c7de34c9
Ahsan280 <ahsanrehman281@gmail.com>
2024-04-19 14:28:12 +0500

    fourth

--- build_files.sh
+++ build_files.sh
@@ -1,5 +1,6 @@
 # build_files.sh
 echo "BUILD START"
+python3.9 -m ensurepip  # Ensure pip is available
 python3.9 -m pip install -r requirements.txt
 python3.9 manage.py collectstatic --noinput --clear
 echo "BUILD END"
