13ce7711f67e7d5ff52212e781841338a50c48c6
Ahsan280 <ahsanrehman281@gmail.com>
2024-04-19 14:50:36 +0500

    fifth

--- build_files.sh
+++ build_files.sh
@@ -1,6 +1,6 @@
 # build_files.sh
 echo "BUILD START"
-python3.10 -m ensurepip  # Ensure pip is available
-python3.10 -m pip install -r requirements.txt
-python3.10 manage.py collectstatic --noinput --clear
+python3.8 -m ensurepip  # Ensure pip is available
+python3.8 -m pip install -r requirements.txt
+python3.8 manage.py collectstatic --noinput --clear
 echo "BUILD END"
